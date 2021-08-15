c110e67a3fab1c980a7e453208bbf962b2c926ab
Junior2k5 <eplj@uol.com.br>
2021-08-15 16:03:45 -0300

    Preparando para publicação no Heroku

--- django2/settings.py
+++ django2/settings.py
@@ -48,15 +48,16 @@ INSTALLED_APPS = [
     'stdimage',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django2.urls'
